6a8a957c7a22e4fb8eb70c7427ca13c1cd3fac6f
User <User@DESKTOP-ITU7A4F>
2022-04-15 15:29:20 -0400

    Add project files.

---
+++ Board.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sudoku_Solver
+{
+    struct Board // 3 x 3 x 3 x 3 (3 x 3 cell) grid
+    {
+        public List<Cell> Data;
+
+
+        public Board(List<Cell> data)
+        {
+            Data = data;
+        }
+        public Board(Board board)
+        {
+            Data = board.Data;
+        }
+        public Board(List<string> data) // assume numbers are separated by spaces and each string is a 9 x 1 (excluding whitespace)
+        {
+            Data = new List<Cell>();
+            foreach (string cell in data)
+            {
+                string[] nums = cell.Split(' ');
+                List<List<Value>> push = new List<List<Value>> {new List<Value>(), new List<Value>(), new List<Value>() };
+                int num = 0;
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        push[i].Add(Value.Int(Convert.ToInt32(nums[num])));
+                        num++;
+                    }
+                }
+                Data.Add(push);
+            }
+        }
+
+        public Board GetCopy()
+        {
+            return new Board(this);
+        }
+        public int Read(Tuple<int, int, int> coord)
+        {
+            return Data[coord.Item1].Data[coord.Item2][coord.Item3].Val;
+        }
+        public void Write(Tuple<int, int, int> coord, Value val)
+        {
+            Data[coord.Item1].Data[coord.Item2][coord.Item3].Val = val.Val;
+        }
+        public bool CheckRow(int row) // 1 based indexing, add 1 to row before call (0 never goes in)
+        {
+            
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            
+            if (row <= 3)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    foreach (Value v in Data[i].Data[(row - 1) % 3])
+                    {
+                        if (v.Val == 0) { continue; }
+                        else if (numbers.Contains(v.Val))
+                        {
+                            numbers.Remove(v.Val);
+                        }
+                        else { return false; }
+                    }
+                }
+            }
+            else if (row <= 6)
+            {
+                for (int i = 3; i < 6; i++)
+                {
+                    foreach (Value v in Data[i].Data[(row - 1) % 3])
+                    {
+                        if (v.Val == 0) { continue; }
+                        else if (numbers.Contains(v.Val))
+                        {
+                            numbers.Remove(v.Val);
+                        }
+                        else { return false; }
+                    }
+                }
+            }
+            else if (row <= 9)
+            {
+                for (int i = 6; i < 9; i++)
+                {
+                    foreach (Value v in Data[i].Data[(row - 1) % 3])
+                    {
+                        if (v.Val == 0) { continue; }
+                        else if (numbers.Contains(v.Val))
+                        {
+                            numbers.Remove(v.Val);
+                        }
+                        else { return false; }
+                    }
+                }
+            }
+
+
+            return true;
+        }
+        public bool CheckColumn(int c) // 1 based indexing
+        {
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            for (int C = (int)Math.Floor( (double)(c - 1) / 3 ); C < 9; C += 3)
+            {
+                foreach (var i in Data[C].Data)
+                {
+                    if (i[(int)Math.Floor((double)(c - 1) % 3)].Val == 0) { continue; }
+                    else if (numbers.Contains(i[(int)Math.Floor((double)(c - 1) % 3)].Val))
+                    {
+                        numbers.Remove(i[(int)Math.Floor((double)(c - 1) % 3)].Val);
+                    }
+                    else { return false; }
+                }
+            }
+            return true;
+        }
+        public bool Solved()
+        {
+            //easy part
+            foreach (Cell cell in Data)
+            {
+                if (!cell.NoDuplicates()) { return false; }
+            }
+            for (int i = 1; i < 10; i++)
+            {
+                if (!CheckRow(i) || !CheckColumn(i)) { return false; }
+            }
+            foreach (Cell c in Data)
+            {
+                foreach (var row in c.Data)
+                {
+                    foreach (var val in row)
+                    {
+                        if (val.Val == 0) { return false;}
+                    }
+                }
+            }
+
+            return true;
+        }
+        public bool IsLegal(Tuple<int, int, int> coord, Value val)
+        {
+            var original = this[coord.ToValueTuple()].Val;
+
+            this[coord.ToValueTuple()].Val = val.Val;
+
+            bool retval = CheckRow(CoordToRow(coord.ToValueTuple())) && CheckColumn(CoordToColumn(coord.ToValueTuple())) && Data[coord.Item1].NoDuplicates();
+            this.Data[coord.Item1].Data[coord.Item2][coord.Item3].Val = original;
+            return retval;
+        }
+        public int CoordToRow((int, int, int) coord)
+        {
+            // (cell // 3) * 3 + 1 + row
+            return ((int)Math.Floor( (double)coord.Item1 / 3 ) * 3 + coord.Item2 + 1);
+        }
+        public int CoordToColumn((int, int, int) coord)
+        {
+            // cell % 3 * 3 + 1 + val
+            return (coord.Item1 % 3) * 3 + 1 + coord.Item3;
+        }
+        
+        public Value this[(int, int, int) pointer]
+        {
+            get => Data[pointer.Item1].Data[pointer.Item2][pointer.Item3];
+            set => Data[pointer.Item1].Data[pointer.Item2][pointer.Item3].Val = value.Val;
+        }
+
+        public void Update()
+        {
+            for (int c = 0; c < Data.Count; c++)
+            {
+                for (int r = 0; r < Data[c].Data.Count; r++)
+                {
+                    for (int v = 0; v < Data[c].Data[r].Count; v++)
+                    {
+                        //Data[c].Data[r][v] is a val
+                        //order doesn't matter for this function
+                        this[(c, r, v)].Works.Clear();
+                        for (int i = 1; i < 10; i++)
+                        {
+                            if (IsLegal((c, r, v).ToTuple(), Value.Int(i))) { this[(c, r, v)].Works.Add(i); }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
---
+++ Solver.cs
@@ -0,0 +1,207 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace Sudoku_Solver
+{
+#pragma warning disable CS0660
+#pragma warning disable CS0661
+    class Value // 1 x 1 square
+#pragma warning restore CS0661
+#pragma warning restore CS0660
+    {
+        public int Val { get; set; }
+        public List<int> Works;
+        public bool access;
+        public Value(int StartVal)
+        {
+            Val = StartVal;
+            Works = new List<int>();
+            access = StartVal == 0;
+        }
+        public static bool operator ==(Value val1, Value val2)
+        {
+            return val1.Val == val2.Val;
+        }
+        public static bool operator !=(Value val1, Value val2)
+        {
+            return val1.Val != val2.Val;
+        }
+        public static Value Int(int num)
+        {
+            return new Value(num);
+        }
+    }
+
+
+    class Solver
+    {
+        public Board board;
+
+        public Solver(Board StartData)
+        {
+            board = StartData;
+        }
+        public void Progress(ref (int, int, int) pointer)
+        {
+            //Console.WriteLine("Progression");
+            if (pointer.Item3 >= 2)
+            {
+                pointer.Item3 = 0;
+                //Console.WriteLine("Pointer.Item3 = 0");
+                if ((pointer.Item1 + 1) % 3 == 0)
+                {
+                    if (pointer.Item2 < 2)
+                    {
+                        pointer.Item1 -= 2;
+                        pointer.Item2++;
+                        pointer.Item3 = 0;
+                  //      Console.WriteLine("Pointer.Item1 -= 1\nPointer.Item2 += 1\nPointer.Item3 = 0");
+                    }
+                    else
+                    {
+                        pointer.Item1++;
+                        pointer.Item2 = 0;
+                        pointer.Item3 = 0;
+                //        Console.WriteLine("Pointer.Item1 += 1\nPointer.Item2 = 0\nPointer.Item3 = 0");
+                    }
+                }
+                else
+                {
+                    pointer.Item1++;
+                  //  Console.WriteLine("Pointer.Item1 += 1");
+                }
+            }
+            else
+            {
+                pointer.Item3++;
+              //  Console.WriteLine("Pointer.Item3 += 1");
+            }
+            //Console.WriteLine(pointer);
+            //Console.Write("\n");
+        }
+        public void Regress(ref (int, int, int) pointer)  
+        {
+            //Console.WriteLine("Regression");
+            if (pointer.Item3 > 0)
+            {
+                pointer.Item3 = 2;
+                //Console.WriteLine("Pointer.Item3 = 2");
+                if (pointer.Item1 % 3 == 0)
+                {
+                    if (pointer.Item2 > 0)
+                    {
+                        pointer.Item1 += 2;
+                        pointer.Item2--;
+                        pointer.Item3 = 2;
+                        //          Console.WriteLine("Pointer.Item1 += 2\nPointer.Item2 -= 1\n Pointer.Item3 = 2");
+                    }
+                    else
+                    {
+                        pointer.Item1--;
+                        //        Console.WriteLine("Pointer.Item1 -= 1");
+                    }
+                }
+                else
+                {
+                    pointer.Item1--;
+                    pointer.Item3 = 2;
+                    //  Console.WriteLine("Pointer.Item1 -= 1\nPointer.Item3 = 2");
+                }
+            }
+            else { pointer.Item3--; }//Console.WriteLine("Pointer.Item3 -= 1"); 
+          //  Console.WriteLine(pointer);
+            //Console.Write("\n");
+        }
+        public Board Solve()
+        {
+            Board temp = board.GetCopy() ; // Safety reasons
+            temp.Update();
+            (int, int, int) pointer = (0, 0, 0);
+            int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            while (!temp.Solved())
+            {
+                //Print(temp);
+                //Console.WriteLine(temp.Solved());
+                //Console.WriteLine(temp[pointer].Val);
+                Console.WriteLine("\n" + Convert.ToString(pointer));
+                Console.Write("Works: ");
+                temp[pointer].Works.ForEach(Console.Write);
+                Console.Write("\n");
+                if (temp.Data[pointer.Item1].Data[pointer.Item2][pointer.Item3].access)
+                {
+                    
+                    Print(temp);
+                    foreach (int i in numbers)
+                    {
+                        if (temp.IsLegal(pointer.ToTuple(), Value.Int(i)) && i >= temp.Read(pointer.ToTuple()) && temp[pointer].access)
+                        {
+                            if (temp[pointer].Val == 9) 
+                            {
+                                if (temp.IsLegal(pointer.ToTuple(), Value.Int(i)))
+                                {
+                                    temp[pointer].Val = 1;
+                                    break;
+                                }
+                                else
+                                {
+                                    temp[pointer].Val = 0; Regress(ref pointer);
+                                    break;
+                                }
+                            }
+                            
+                            Console.WriteLine(temp.CheckRow(1));
+                            Console.WriteLine("Is legal: " + temp.IsLegal(pointer.ToTuple(), Value.Int(i)).ToString());
+                            temp.Write(pointer.ToTuple(), Value.Int(i));
+                            Console.WriteLine("Data[" + Convert.ToString(pointer) + "] = " + Convert.ToString(i));
+                            Progress(ref pointer);
+                            break;
+                        }
+                        else if (temp.Data[pointer.Item1].Data[pointer.Item2][pointer.Item3].Works.Count == 0)
+                        {
+                            temp.Write(pointer.ToTuple(), Value.Int(0));
+                            Console.WriteLine(pointer.ToString() + " = " + 0.ToString());
+                            do { Regress(ref pointer); } while (!temp.Data[pointer.Item1].Data[pointer.Item2][pointer.Item3].access || temp[pointer].Val == 9);
+                            break;
+                        }
+                    }
+                    
+                }
+                else { Progress(ref pointer); }
+                temp.Update();
+            }
+            return temp;
+        }
+        public void Print(Board board)
+        {
+            (int, int, int) pointer = (0, 0, 0);
+            Console.WriteLine("--------------------------");
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    for (int x = 0; x < 3; x++)
+                    {
+                        for (int y = 0; y < 3; y++)
+                        {
+                            //Console.WriteLine(pointer);
+                            Console.Write(Convert.ToString(board[pointer].Val) + " ");
+                            Progress(ref pointer);
+
+                        }
+                        Console.Write(" | ");
+                    }
+                    Console.Write("\n");
+                }
+                Console.WriteLine("--------------------------");
+            }
+        }
+        public void Run()
+        {
+            board = Solve();
+            Console.WriteLine("\n\n");
+            Print(board);
+        }
+    }
+}
+
